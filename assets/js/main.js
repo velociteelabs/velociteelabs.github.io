72935e2bf62af7e842b6a4ab73463624685cd2af
nganesh197 <nganesh197@users.noreply.github.com>
2020-06-23 22:39:03 -0700

    Secondary Updates
    Added in text. Good starting point for website.

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'images/motorcycle_riding.jpg': 'center',
+							'images/motorcycle.jpg': 'center',
+							'images/Titlepage_pic.png': 'center'
 						},
 
 					// Delay.
